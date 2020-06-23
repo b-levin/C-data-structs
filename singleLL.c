07f18bc32871eaa470a679fcbf40f08c925b1b6e
blevin <windrider417@gmail.com>
2020-06-22 20:54:05 -0400

    added linked list methods

--- singleLL.c
+++ singleLL.c
@@ -9,3 +9,32 @@ struct sLinkedList* initSLL() {
     return out;
 }
 
+//adds an item to the linked list at index
+int add(struct sLinkedList* list, int item, int index) {
+    return -1;
+}
+
+//removes and item at index from linked list
+int remove(struct sLinkedList* list, int index) {
+    return -1;
+}
+
+//gets value of item at index
+int get(struct sLinkedList* list, int index) {
+    return -1;
+}
+
+//sets the value of item and index to new item
+int set(struct sLinkedList* list, int index, int item) {
+    return -1;
+}
+
+//returns the size of the linked list
+int size(struct sLinkedList* list) {
+    return list->size;
+}
+
+//returns a pointer to the linked list in string form
+char *toString(struct sLinkedList* list) {
+    return "";
+}
